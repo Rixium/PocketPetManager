4cb3af907e8755d4fb8fce8ac97ebd54417b5f5a
Rixium <gamerpotty@gmail.com>
2021-03-22 18:24:54 +0000

    create module locator

---
+++ src/Client/Modules/ModuleLocators.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Client.Modules
+{
+    internal class ModuleLocators : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        public ModuleLocators(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider) =>
+            _regionManager.RegisterViewWithRegion("Shell", typeof(Controls.WelcomeControl));
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
