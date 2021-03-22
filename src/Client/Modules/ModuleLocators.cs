f1be88718d833f4e62c5e0412ac58b96d09e1949
Rixium <gamerpotty@gmail.com>
2021-03-22 19:54:46 +0000

    register new form with region manager

--- src/Client/Modules/ModuleLocators.cs
+++ src/Client/Modules/ModuleLocators.cs
@@ -14,8 +14,11 @@ namespace Client.Modules
             _regionManager = regionManager;
         }
 
-        public void OnInitialized(IContainerProvider containerProvider) =>
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
             _regionManager.RegisterViewWithRegion("Shell", typeof(Welcome));
+            _regionManager.RegisterViewWithRegion("Shell", typeof(NewPet));
+        }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
