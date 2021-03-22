c8577d7b1c0c6b43ad3c9177d251d32872ec23dd
Rixium <gamerpotty@gmail.com>
2021-03-22 21:42:10 +0000

    make use of item store in service

--- src/Client/Services/ItemService.cs
+++ src/Client/Services/ItemService.cs
@@ -1,4 +1,7 @@
-﻿using Client.Builders;
+﻿using System.Collections.Generic;
+using Client.Builders;
+using Client.Models;
+using Client.Persistence;
 using JetBrains.Annotations;
 
 namespace Client.Services
@@ -6,9 +9,24 @@ namespace Client.Services
     [UsedImplicitly]
     internal class ItemService : IItemService
     {
+        private readonly IItemStore _itemStore;
+
+        public ItemService(IItemStore itemStore)
+        {
+            _itemStore = itemStore;
+        }
+
+        public IReadOnlyCollection<Item> GetItems() => _itemStore.Get();
+
         public void CreateNewItem(IItemBuilder itemBuilder)
         {
+            var items = GetItems();
+
+            itemBuilder.SetItemId(items.Count + 1);
+
             var item = itemBuilder.Build();
+            _itemStore.Add(item);
+            _itemStore.Save();
         }
     }
 }
