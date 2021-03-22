d24a844d7716b84b9fa051b0c33576e9c86806d7
Rixium <gamerpotty@gmail.com>
2021-03-22 22:14:22 +0000

    display existing items in view all

---
+++ src/Client/ViewModels/ViewAllViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Client.Models;
+using Client.Services;
+using JetBrains.Annotations;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace Client.ViewModels
+{
+    [UsedImplicitly]
+    internal class ViewAllViewModel : BindableBase
+    {
+        private readonly IItemService _itemService;
+
+        public ViewAllViewModel(IItemService itemService) => _itemService = itemService;
+
+        private IReadOnlyCollection<Item> _items;
+
+        public IReadOnlyCollection<Item> Items
+        {
+            get => _items;
+            set => SetProperty(ref _items, value);
+        }
+
+        public DelegateCommand Refresh => new DelegateCommand(() => Items = _itemService.GetItems());
+    }
+}
