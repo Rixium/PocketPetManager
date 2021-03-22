94e4e771fb01ae064daa674786ca12ebbb1ec8a1
Rixium <gamerpotty@gmail.com>
2021-03-22 22:14:09 +0000

    add get items to item service interface

--- src/Client/Services/IItemService.cs
+++ src/Client/Services/IItemService.cs
@@ -1,9 +1,12 @@
-﻿using Client.Builders;
+﻿using System.Collections.Generic;
+using Client.Builders;
+using Client.Models;
 
 namespace Client.Services
 {
     public interface IItemService
     {
+        IReadOnlyCollection<Item> GetItems();
         void CreateNewItem(IItemBuilder itemBuilder);
     }
 }
