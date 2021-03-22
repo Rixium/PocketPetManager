1a88227192fd7fd6d59d6fe9c7946b41a0e50ed1
Rixium <gamerpotty@gmail.com>
2021-03-22 21:48:04 +0000

    add navigation pane

---
+++ src/Client/ViewModels/NavigationPaneViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Client.Views;
+using JetBrains.Annotations;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace Client.ViewModels
+{
+    [UsedImplicitly]
+    internal class NavigationPaneViewModel : BindableBase
+    {
+        private readonly IRegionManager _regionManager;
+
+        public DelegateCommand CreateNewPetCommand =>
+            new(() => _regionManager.RequestNavigate("Shell", nameof(NewPet)));
+
+        public NavigationPaneViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+    }
+}
---
+++ src/Client/Views/NavigationPane.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Client.Views
+{
+    public partial class NavigationPane
+    {
+        public NavigationPane()
+        {
+            InitializeComponent();
+        }
+    }
+}
