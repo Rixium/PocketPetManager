95e3b7712be120ad1a39ffa99ee7133951c6b500
Rixium <gamerpotty@gmail.com>
2021-03-22 23:20:36 +0000

    add loading json documents rather than have static location

--- src/Client/ViewModels/MainWindowViewModel.cs
+++ src/Client/ViewModels/MainWindowViewModel.cs
@@ -9,12 +9,29 @@ namespace Client.ViewModels
     internal class MainWindowViewModel
     {
         private readonly IItemStore _itemStore;
+        public DelegateCommand LoadItemsCommand => new(LoadItems);
         public DelegateCommand SaveAsJsonCommand => new(SaveAsJson);
         public DelegateCommand ExportAsLuaCommand => new(ExportAsLua);
 
         public MainWindowViewModel(IItemStore itemStore)
         {
             _itemStore = itemStore;
+        }
+
+        private void LoadItems()
+        {
+            var dialog = new OpenFileDialog
+            {
+                DefaultExt = ".json",
+                Filter = "JSON text documents (.json) | *.json"
+            };
+
+            var result = dialog.ShowDialog();
+
+            if (result != true) return;
+
+            var filename = dialog.FileName;
+            _itemStore.ReadFromFile(filename);
         }
 
         private void SaveAsJson()
