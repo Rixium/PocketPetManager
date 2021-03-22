d469d2ef5f52e33677f99037a278fc3354cd222b
Rixium <gamerpotty@gmail.com>
2021-03-22 19:54:34 +0000

    add basic form to specify pet properties

---
+++ src/Client/ViewModels/NewPetViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace Client.ViewModels
+{
+    internal class NewPetViewModel : BindableBase
+    {
+        public static string[] ItemTypes => new[]
+        {
+            "Seed",
+            "Pet",
+            "Coin"
+        };
+
+        public string ItemName { get; set; }
+
+        public static string[] PetTypes => new[]
+        {
+            "Pixie",
+            "Cool",
+            "Brute"
+        };
+
+        public string Description { get; set; }
+
+        public string ModelId { get; set; }
+
+        public string ExperienceToLevel { get; set; }
+
+        public string LevelToEvolution { get; set; }
+        
+        public DelegateCommand SelectEvolutionCommand => new(() => { });
+
+        public DelegateCommand SaveItemCommand => new(SaveItem);
+
+        private void SaveItem()
+        {
+            Console.WriteLine("SAVING");
+        }
+    }
+}
---
+++ src/Client/Views/NewPet.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Client.Views
+{
+    public partial class NewPet : UserControl
+    {
+        public NewPet()
+        {
+            InitializeComponent();
+        }
+    }
+}
