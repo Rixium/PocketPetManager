7250aecd9048ac8feeed4cc5a603cb7824320dd9
Rixium <gamerpotty@gmail.com>
2021-03-22 20:22:17 +0000

    create item builder

---
+++ src/Client/Builders/IItemBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Client.Models;
+
+namespace Client.Builders
+{
+    public interface IItemBuilder
+    {
+        void SetItemType(string type);
+        void SetName(string name);
+        void SetType(string type);
+        void SetDescription(string description);
+        void SetModelId(string modelId);
+        void SetExperienceToLevel(string experienceToLevel);
+        void SetLevelToEvolve(string levelToEvolve);
+        void SetEvolvesTo(string evolvesTo);
+        Item Build();
+    }
+}
---
+++ src/Client/Builders/ItemBuilder.cs
@@ -0,0 +1,63 @@
+﻿using Client.Models;
+using JetBrains.Annotations;
+
+namespace Client.Builders
+{
+    [UsedImplicitly]
+    public class ItemBuilder : IItemBuilder
+    {
+        private int _id;
+        private string _itemType;
+        private string _name;
+        private string _type;
+        private string _description;
+        private int? _modelId;
+        private int? _experienceToLevel;
+        private int? _levelToEvolve;
+        private int? _evolvesTo;
+
+        public void SetItemId(int id) => _id = id;
+
+        public void SetItemType(string type) => _itemType = type;
+
+        public void SetName(string name) => _name = name;
+
+        public void SetType(string type) => _type = type;
+
+        public void SetDescription(string description) => _description = description;
+
+        public void SetModelId(string modelId) => _modelId = Parse(modelId);
+
+        public void SetExperienceToLevel(string experienceToLevel) => _experienceToLevel = Parse(experienceToLevel);
+
+        public void SetLevelToEvolve(string levelToEvolve) => _levelToEvolve = Parse(levelToEvolve);
+
+        public void SetEvolvesTo(string evolvesTo) => _evolvesTo = Parse(evolvesTo);
+
+        public Item Build() =>
+            new Item
+            {
+                ItemId = _id,
+                ItemType = _itemType,
+                Name = _name,
+                Type = _type,
+                Description = _description,
+                ModelId = _modelId,
+                ExperienceToLevel = _experienceToLevel,
+                LevelToEvolve = _levelToEvolve,
+                EvolvesTo = _evolvesTo
+            };
+
+        private static int? Parse(string s)
+        {
+            var parsed = int.TryParse(s, out var result);
+
+            if (!parsed)
+            {
+                return null;
+            }
+
+            return result;
+        }
+    }
+}
