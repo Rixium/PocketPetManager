30db0f0ef4ca25bd9dcfd6e8cf8ecde880d94b89
Rixium <gamerpotty@gmail.com>
2021-03-22 20:22:30 +0000

    make some fields on item nullable

--- src/Client/Models/Item.cs
+++ src/Client/Models/Item.cs
@@ -1,15 +1,15 @@
 ﻿namespace Client.Models
 {
-    internal class Item
+    public class Item
     {
         public int ItemId { get; init; }
         public string ItemType { get; init; }
         public string Name { get; init; }
         public string Type { get; init; }
         public string Description { get; init; }
-        public int ModelId { get; init; }
-        public int ExperienceToLevel { get; init; }
-        public int LevelToEvolve { get; init; }
-        public int EvolvesTo { get; init; }
+        public int? ModelId { get; init; }
+        public int? ExperienceToLevel { get; init; }
+        public int? LevelToEvolve { get; init; }
+        public int? EvolvesTo { get; init; }
     }
 }
