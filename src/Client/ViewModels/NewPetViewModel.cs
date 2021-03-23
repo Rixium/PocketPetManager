4b2ac6b81f5cf10b0ad974fe54a6b116cec21739
Rixium <gamerpotty@gmail.com>
2021-03-23 00:07:17 +0000

    update new pet form styling

--- src/Client/ViewModels/NewPetViewModel.cs
+++ src/Client/ViewModels/NewPetViewModel.cs
@@ -169,7 +169,6 @@ namespace Client.ViewModels
         {
             if (navigationContext.Parameters["Item"] == null)
             {
-                _itemBuilder = _itemBuilder.New();
                 return;
             }
 
